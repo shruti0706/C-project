17c6b0a85ebb3bd000ebdd45131c213666c603bb
shruti0706 <72216138+shruti0706@users.noreply.github.com>
2021-09-07 15:24:34 +0530

    --- Auto-Git Commit ---

--- project.c
+++ project.c
@@ -1,1 +1,58 @@
-include<stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+void Welcomepage();
+void Loginpage();
+void AddProduct();
+void EditProduct();
+void DisplayAllProducts();
+void SearchEntry();
+void DeleteProduct();
+void CalculateBill();
+int main(){
+
+    return 0;
+}
+void Welcomepage(){
+
+
+
+}
+void Loginpage(){
+
+
+
+}
+void AddProduct(){
+
+
+
+}
+void EditProduct(){
+
+
+
+}
+void DisplayAllProducts(){
+
+
+
+}
+void SearchEntry(){
+
+
+
+}
+void DeleteProduct(){
+
+
+
+}
+void CalculateBill(){
+
+
+
+    
+}
