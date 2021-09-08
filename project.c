61c260d9c579c06b08fdc703eddecf15edae421d
shruti0706 <72216138+shruti0706@users.noreply.github.com>
2021-09-08 10:28:43 +0530

    --- Auto-Git Commit ---

--- project.c
+++ project.c
@@ -1,75 +1,119 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+struct loginCredential
+{
+  char userid[50];
+  char password[50];
+} login[100];
 
-void Welcomepage();
-void MainMenu();
-void Loginpage();
-void AddProduct();
-void EditProduct();
-void DisplayAllProducts();
-void SearchEntry();
-void DeleteProduct();
-void CalculateBill();
-int main(){
-    
+void welcomepage();
+void mainMenu(int i);
+void loginpage();
+void addProduct();
+void editProduct();
+void displayAllProducts();
+void searchEntry();
+void deleteProduct();
+void calculateBill(); 
+int main()
+{
+
   //Welcomepage();
-
-    return 0;
-
+  MainMenu();
+  return 0;
 }
-void Welcomepage(){
-    
-        printf("************************************                                           ");
-        
-        printf("\n\n\n\n\n\n                :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n                ::                                                                 ::");
-       
-           printf("\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                          FASHION HUB                            ::  \n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                  A style for every story...     ::\n                ");
-            printf(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
-        printf("\n\n\n\n\n\n\n");
-       printf("************************************                                           ");
-         
-    
-} 
-void MainMenu(){
+void welcomepage()
+{
 
+  printf("************************************                                           ");
 
-    
+  printf("\n\n\n\n\n\n                :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n                ::                                                                 ::");
+
+  printf("\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                          FASHION HUB                            ::  \n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                                                 ::\n                ::                                  A style for every story...     ::\n                ");
+  printf(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+  printf("\n\n\n\n\n\n\n");
+  printf("************************************                                           ");
 }
-void Loginpage(){
+void mainMenu(int i)
+{
+
+  int choice;
+  while (1)
+  {
+    if(i==1){
+    printf("\n\t\t\t        ++++++++++++++++++++++++++++++++++++++++++++++++      ");
+    printf("\n\t\t\t\t+   WELCOME CLOTHING STORE MANAGEMENT SYSTEM   +      ");
+    printf("\n\t\t\t        ++++++++++++++++++++++++++++++++++++++++++++++++      ");
+    printf("                                                                                          ");
+    printf("                                                                                          ");
+    printf("\n\t\t\t\t FEATURE OF CLOTHING STORE\n\t\t\t\t ________________");
+    printf("\n\t\t\t\t\t\t MAIN MENU\n\t\t\t\t\t\t............");
+    printf("\n\t\t\t\t| [1] ADD_PRODUCT                              |");
+    printf("\n\t\t\t\t| _______________|");
+    printf("\n\t\t\t\t| [2] DELETE_PRODUCT                           |");
+    printf("\n\t\t\t\t| _______________|");
+    printf("\n\t\t\t\t| [3] SEARCH_PRODUCT                           |");
+    printf("\n\t\t\t\t| _______________|");
+    printf("\n\t\t\t\t| [4] DISPLAY_PRODUCT                          |");
+    printf("\n\t\t\t\t| _______________|");
+    printf("\n\t\t\t\t| [5] EDIT_PRODUCT                             |");
+    printf("\n\t\t\t\t| _______________|");
+    printf("\n\t\t\t\t| [6] EXIT                                     |\n\t\t\t\t__________________\n\t\t\t\t\t\t\t\t");
+    printf("\n\t\t\t\t|ENTER CHOICE\n\n");
+
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+      Add_Product();
+      break;
+    case 2:
+      Delete_Product();
+      break;
+    case 3:
+      Search_Product();
+      break;
+    case 4:
+      Display_Product();
+      break;
+    case 5:
+      Edit_Product();
+      break;
+    case 6:
+      Exit(0);
+    }
+  }
+  }
+  if(i==2){
 
 
 
+
+
+
+
+  }
 }
-void AddProduct(){
-
-
-
+void loginpage()
+{
 }
-void EditProduct(){
-
-
-
+void addProduct()
+{
 }
-void DisplayAllProducts(){
-
-
-
+void editProduct()
+{
 }
-void SearchEntry(){
-
-
-
+void displayAllProducts()
+{
 }
-void DeleteProduct(){
-
-
-
+void searchEntry()
+{
 }
-void CalculateBill(){
-
-
-
-    
+void deleteProduct()
+{
+}
+void calculateBill()
+{
 }
